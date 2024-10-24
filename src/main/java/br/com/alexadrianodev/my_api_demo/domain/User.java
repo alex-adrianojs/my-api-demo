393684784b244d9a185cc31016bd9ae77d86d8f4
Alex Adriano de Jesus  Souza <alexadriano.dev@gmail.com>
2024-10-24 08:42:28 -0300

    User class created

---
+++ src/main/java/br/com/alexadrianodev/my_api_demo/domain/User.java
@@ -0,0 +1,24 @@
+package br.com.alexadrianodev.my_api_demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
