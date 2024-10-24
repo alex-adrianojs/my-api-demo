55c97623b210bf9999c33e636f1829ab7927c816
Alex Adriano de Jesus  Souza <alexadriano.dev@gmail.com>
2024-10-24 08:30:29 -0300

    Project started

---
+++ src/main/java/br/com/alexadrianodev/my_api_demo/MyApiDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexadrianodev.my_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alexadrianodev/my_api_demo/MyApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexadrianodev.my_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
